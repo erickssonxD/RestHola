288e3234b97b8ddd5655d49b6a27be303c7a0dc9
Erick <ekmoya@proton.me>
2023-11-19 18:12:00 -0300

    [NH] :hammer: Create remove method. Working.

--- src/main/java/com/iplacex/resthola/HolaService.java
+++ src/main/java/com/iplacex/resthola/HolaService.java
@@ -14,11 +14,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 import java.util.Optional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import com.google.gson.stream.JsonReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+import javax.ws.rs.DELETE;
 
 @Path("/hola")
 public class HolaService {
@@ -30,13 +39,69 @@ public class HolaService {
         public ErrorResponse(String message) {
             this.message = message;
         }
+    }
 
-        // You can add getters/setters if needed
+    /*
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createInsumo(Insumo insumo) {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+
+        try {
+            String jsonResponse = gson.toJson(insumo);
+            return Response.status(201).entity(jsonResponse).build();
+        } catch (Exception e) {
+            ErrorResponse response = new ErrorResponse("Error processing the request");
+            String jsonResponse = gson.toJson(response);
+            return Response.status(500).entity(jsonResponse).build();
+        }
+    }
+     */
+    @DELETE
+    @Path("/{codigo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response eliminarInsumo(@PathParam("codigo") int codigo) throws IOException {
+
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        try {
+            JsonReader reader = new JsonReader(new FileReader("productos.json"));
+            List<Insumo> insumos = gson.fromJson(reader, new TypeToken<List<Insumo>>() {
+            }.getType());
+
+            boolean removed = false;
+            Iterator<Insumo> iterator = insumos.iterator();
+            while (iterator.hasNext()) {
+                Insumo insumo = iterator.next();
+                if (insumo.getCodigo() == codigo) {
+                    iterator.remove();
+                    removed = true;
+                }
+            }
+
+            if (removed) {
+                try (Writer writer = new FileWriter("productos.json")) {
+                    gson.toJson(insumos, writer);
+                }
+
+                ErrorResponse response = new ErrorResponse("remove successfully");
+                String jsonResponse = gson.toJson(response);
+                return Response.status(200).entity(jsonResponse).build();
+            } else {
+                ErrorResponse response = new ErrorResponse("product code does not exist");
+                String jsonResponse = gson.toJson(response);
+                return Response.status(409).entity(jsonResponse).build();
+            }
+        } catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+            ErrorResponse response = new ErrorResponse("error reading 'productos.json'");
+            String jsonResponse = gson.toJson(response);
+            return Response.status(500).entity(jsonResponse).build();
+        }
     }
 
     @GET
     @Path("/{codigo}")
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response consultarInsumo(@PathParam("codigo") int codigo) {
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         try {
@@ -49,7 +114,7 @@ public class HolaService {
             for (Insumo insumo : insumos) {
                 if (insumo.getCodigo() == codigo) {
                     foundInsumo = insumo;
-                    break; // Exit loop once the desired insumo is found
+                    break;
                 }
             }
 
@@ -70,7 +135,7 @@ public class HolaService {
 
     @GET
     @Path("/")
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response consultarInsumos() {
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
         try {
